928d831ccb60e8612c83ba838018a1f741c7bc0a
Ryhor Mudrahel <ryhor.murahel@gmail.com>
2023-10-19 19:47:08 -0400

    Added simple custom login page

---
+++ src/main/java/rm/jobseeker/contollers/LoginController.java
@@ -0,0 +1,13 @@
+package rm.jobseeker.contollers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public  String login(){
+        return "login";
+    }
+}
